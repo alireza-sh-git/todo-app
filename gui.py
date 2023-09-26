41f3ca8bda32261002da94c53fcda5af94d4aa1c
alireza_rmc <sheikhi.l.alireza@gmail.com>
2023-09-26 20:53:27 +0330

    add all buttons to gui and fixed the size/font. coded the add, save and exit buttons.

--- gui.py
+++ gui.py
@@ -2,10 +2,40 @@ import todo_functions
 import PySimpleGUI as sg
 
 
+todos = todo_functions.get_todos()
+
 label = sg.Text("Please enter a todo: ")
-inputBox = sg.InputText(tooltip="Enter todo")
+inputBox = sg.InputText(tooltip="Enter todo", key="todo")
 addButton = sg.Button("Add")
-window = sg.Window("My To-Do App", layout=[[label], [inputBox, addButton]])
-window.read()
-print("Hello")
+editButton = sg.Button("Edit")
+deleteButton = sg.Button("Delete")
+saveButton = sg.Button("Save")
+exitButton = sg.Button("Exit")
+listBox = sg.Listbox(todos, size=(40, 10), key="todos")
+window = sg.Window("My To-Do App",
+                   layout=[[label], [inputBox, addButton],
+                           [listBox, editButton, deleteButton],
+                           [saveButton, exitButton]],
+                   font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todo = values["todo"].strip()
+            if todo == "":
+                print("Please enter a todo")
+            else:
+                todo_functions.add_todo(todos, todo)
+                listBox.update(todos)
+        case "Edit":
+            break
+        case "Delete":
+            break
+        case "Save":
+            todo_functions.save_todos(todos)
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
